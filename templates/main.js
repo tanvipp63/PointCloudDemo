2958eaa0154fbb8f836fb16a18494bb172c8455b
tanvi parulekar <s4703141@student.uq.edu.au>
2025-07-04 19:03:59 +1000

    Basic cube animation

--- templates/main.js
+++ templates/main.js
@@ -1,1 +1,26 @@
 import * as THREE from 'three';
+/* Set up scene, camera and renderer */
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animate );
+document.body.appendChild( renderer.domElement );
+
+/* Dummy animation object */
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+camera.position.z = 5; /* Offset camera so it is not on top of the cube */
+
+/*Animation function */
+function animate() {
+
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
+
+}
