32e5d125c5d5220c89652247577bd204e27933fe
tanvi parulekar <s4703141@student.uq.edu.au>
2025-08-11 13:15:46 +1000

    Got Three js to display with button. Forgot to mention in main commit that pose interpolation with SQUAD is still buggy

--- templates/main.js
+++ templates/main.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'https://threejs.org/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'https://unpkg.com/three@0.152.2/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.152.2/examples/jsm/controls/OrbitControls.js?module';
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
